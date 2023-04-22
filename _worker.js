169d37f95f1ff7353b8ccda882acb885819137b2
aiyoubenrenlaiye <33372654+aiyoubenrenlaiye@users.noreply.github.com>
2023-04-22 12:23:27 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    url.host = "https://chat-gpt-next-web-go8f-k38gnj877-aiyoubenrenlaiye.vercel.app/";
+    url.host = "chat-gpt-next-web-go8f-k38gnj877-aiyoubenrenlaiye.vercel.app";
     // openai is already set all CORS heasders 
     return fetch(url, {
       headers: request.headers,
